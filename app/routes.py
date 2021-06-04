799f5d19ce9c7b6f11dc44df7cae5c502c0246b0
Dena Burd <denaburd@gmail.com>
2021-06-04 12:35:24 -0700

    adds get planet by id route

--- app/routes.py
+++ app/routes.py
@@ -5,15 +5,35 @@ from flask import request, Blueprint, make_response, jsonify
 planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 
-@planet_bp.route("", methods=["GET"])
+@planet_bp.route("/", methods=["GET", "POST"])
 def handle_planets():
-    planets = Planet.query.all()
-    planets_response = []
-    for planet in planets:
-        planets_response.append({
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description,
-            "has_rings": planet.has_rings
-        })
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "has_rings": planet.has_rings
+            })
         return jsonify(planets_response)
+    elif request.method == 'POST':
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["name"],
+                            description=request_body["description"], has_rings=request_body["has_rings"])
+        db.session.add(new_planet)
+        db.session.commit()
+        return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+
+@planet_bp.route("/<planet_id>", methods=["GET"])
+def handle_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+
+    return{
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "has_rings": planet.has_rings
+    }
